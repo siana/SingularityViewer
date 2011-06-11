c609e09709c0255811d6d787fded20e4a28d0589
Xara <xara.margot@gmail.com>
2011-06-11 16:40:05 -0700

    Fix for hungry CPU search truc bidouille, bug introducted in snow 1.36.**

--- indra/newview/llfloaterdirectory.cpp
+++ indra/newview/llfloaterdirectory.cpp
@@ -309,7 +309,7 @@ void* LLFloaterDirectory::createGroupDetailHolder(void* userdata)
 void* LLFloaterDirectory::createPlaceDetail(void* userdata)
 {
 	LLFloaterDirectory *self = (LLFloaterDirectory*)userdata;
-	self->mPanelPlacep = new LLPanelPlace();
+	self->mPanelPlacep = new LLPanelPlace(false);
 	LLUICtrlFactory::getInstance()->buildPanel(self->mPanelPlacep, "panel_place.xml");
 	self->mPanelPlacep->setVisible(FALSE);
 	return self->mPanelPlacep;
@@ -319,7 +319,7 @@ void* LLFloaterDirectory::createPlaceDetail(void* userdata)
 void* LLFloaterDirectory::createPlaceDetailSmall(void* userdata)
 {
 	LLFloaterDirectory *self = (LLFloaterDirectory*)userdata;
-	self->mPanelPlaceSmallp = new LLPanelPlace();
+	self->mPanelPlaceSmallp = new LLPanelPlace(false);
 	LLUICtrlFactory::getInstance()->buildPanel(self->mPanelPlaceSmallp, "panel_place_small.xml");
 	self->mPanelPlaceSmallp->setVisible(FALSE);
 	return self->mPanelPlaceSmallp;
@@ -453,7 +453,7 @@ void LLFloaterDirectory::toggleFind(void*)
 	}
 	else
 	{
-		sInstance->close();
+		sInstance->setVisible(FALSE);
 	}
 }
 
@@ -477,7 +477,7 @@ void LLFloaterDirectory::toggleEvents(void*)
 		LLPanel *current_panel = sInstance->childGetVisibleTab("Directory Tabs");
 		if (current_panel == sInstance->mEventsPanel)
 		{
-			sInstance->close();
+			sInstance->setVisible(FALSE);
 		}
 		else
 		{
@@ -491,7 +491,7 @@ void LLFloaterDirectory::hide(void*)
 {
 	if (sInstance)
 	{
-		sInstance->close();
+		sInstance->setVisible(FALSE);
 	}
 }
 
@@ -504,7 +504,8 @@ void LLFloaterDirectory::setVisible(BOOL visible)
 
 void LLFloaterDirectory::onClose(bool app_quitting)
 {
-	setVisible(FALSE);
+//	setVisible(FALSE);
+	LLFloater::onClose(app_quitting);	// so that the web browsers are destroyed and stop eating up 100% of the CPU...
 }
 
 // static
